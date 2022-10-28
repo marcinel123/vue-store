2a4fc819c11066ad4e1f827e77b93d3a6ff4d80a
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-10-28 08:34:16 +0200

    creating next components

---
+++ src/components/ProductsPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="product-list">
+    <h3>Products List</h3>
+    <div v-for="product in products" :key="product.id">
+    <p>{{product.title}}</p>
+    <p>{{product.price}}</p>
+    <p>{{product.description}}</p>
+    <img width="200" height="200" :src="`${product.image}`" :alt="`${product.title}`">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props: ["products"],
+ setup(props) {
+  console.log(props.products)
+ }
+}
+</script>
+
+<style>
+
+</style>
