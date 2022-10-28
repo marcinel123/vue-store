47ce55ee4490a052d29f45fe24c4381edde574c8
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-10-28 07:21:22 +0200

    starting vue project

---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    Hello! My name is {{name}} and my age is {{age}}
+    </template>
+    
+    <script>
+    
+    export default {
+      name: 'HomePage',
+      setup() {
+        
+        return {}
+      }
+      
+    }
+    </script>
+    
+    <style>
+    
+    </style>
+    
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "../components/HomePage"
+
+const routes = [
+    {
+        path: "/",
+        name: "HomePage",
+        component: HomePage
+
+    }
+
+
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes
+})
+
+export default router
