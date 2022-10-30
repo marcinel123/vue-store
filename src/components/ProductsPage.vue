78ee582349bc797fdb22850292dbfd09da2e3080
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-10-30 13:25:00 +0100

    small changes

--- src/components/ProductsPage.vue
+++ src/components/ProductsPage.vue
@@ -1,24 +1,52 @@
 <template>
   <div class="product-list">
     <h3>Products List</h3>
-    <div v-for="product in products" :key="product.id">
-    <p>{{product.title}}</p>
-    <p>{{product.price}}</p>
-    <p>{{product.description}}</p>
-    <img width="200" height="200" :src="`${product.image}`" :alt="`${product.title}`">
+    <button @click="items = mens">Men's clothing</button>
+    <button @click="items = jewelery">Jewelery</button>
+    <button @click="items = electronics">Electronics</button>
+    <button @click="items = women">Women's clothing</button>
+    <div v-for="product in items" :key="product.id">
+      <SingleProductPage :product="product" />
     </div>
   </div>
 </template>
 
 <script>
+import { computed, ref } from "@vue/runtime-core";
+import SingleProductPage from "./SingleProductPage.vue";
 export default {
- props: ["products"],
- setup(props) {
-  console.log(props.products)
- }
-}
+  components: { SingleProductPage },
+  props: ["products"],
+  setup(props) {
+    let items = ref(props.products);
+
+    const jewelery = computed(() => {
+      return props.products.filter(
+        (product) => product.category === "jewelery"
+      );
+    });
+
+    const mens = computed(() => {
+      return props.products.filter(
+        (product) => product.category === "men's clothing"
+      );
+    });
+
+    const electronics = computed(() => {
+      return props.products.filter(
+        (product) => product.category === "electronics"
+      );
+    });
+
+    const women = computed(() => {
+      return props.products.filter(
+        (product) => product.category === "women's clothing"
+      );
+    });
+
+    return { mens, items, women, jewelery, electronics };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/SingleProductPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>{{product.title}}</p>
+    <p>Price: {{product.price}}</p>
+    <p>{{snippet}}</p>
+    <img width="200" height="200" 
+    :src="`${product.image}`" 
+    :alt="`${product.title}`">
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core'
+export default {
+    props: ["product"],
+    setup(props) {
+
+        const snippet = computed(()=>{
+            return props.product.description.substr(0,50) + "..."
+        })
+
+
+        return {snippet}
+    }
+
+}
+</script>
+
+<style>
+
+</style>
